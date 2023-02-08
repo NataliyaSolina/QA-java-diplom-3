5a77ea38bae02089673fc48c1800d63c7bde4157
Nataliya <nataliya.solina@bsc-ideas.com>
2023-02-08 22:04:23 +0300

    Add class property

---
+++ src/main/java/org/example/Property.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Property {
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+    public static final String DRIVER_PATH = "C:\\WebDriver\\bin\\yandexdriver.exe";
+    public static final String PATH_REGISTER = "/register";
+    public static final String PATH_LOGIN = "/login";
+    public static final String PATH_MAIN = "/";
+    public static final String PATH_FOGOT_PASS = "/forgot-password";
+    public static final String PATH_PERSONAL_ACCOUNT = "/account/profile";
+    public static final String NAME = "Tasha";
+    public static final String EMAIL = "Tasha261180@yandex.ru";
+    public static final String PASSWORD = "261180";
+}
