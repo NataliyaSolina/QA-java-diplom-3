486157cb3e49edd6a7547b2cff5e2d3cadc8886f
Nataliya <nataliya.solina@bsc-ideas.com>
2023-02-13 13:39:03 +0300

    Changes by basic comment about hardCode

--- src/main/java/org/example/Property.java
+++ src/main/java/org/example/Property.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.Random;
+
 public class Property {
     public static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
     public static final String DRIVER_PATH = "C:\\WebDriver\\bin\\yandexdriver.exe";
@@ -8,7 +12,12 @@ public class Property {
     public static final String PATH_MAIN = "/";
     public static final String PATH_FOGOT_PASS = "/forgot-password";
     public static final String PATH_PERSONAL_ACCOUNT = "/account/profile";
-    public static final String NAME = "Tasha";
-    public static final String EMAIL = "Tasha261180@yandex.ru";
-    public static final String PASSWORD = "261180";
+    public static final String NAME = RandomStringUtils.randomAlphabetic(3, 5);
+    public static final String EMAIL = RandomStringUtils.randomAlphanumeric(3, 9) + emailRandom();
+    public static final String PASSWORD = RandomStringUtils.randomAlphanumeric(6, 9);
+    public static String emailRandom() {
+        String[] mails = new String[]{"@yandex.ru", "@ya.ru", "@gmail.com", "@mail.ru"};
+        int randomMails = new Random().nextInt(mails.length);
+        return mails[randomMails];
+    }
 }
